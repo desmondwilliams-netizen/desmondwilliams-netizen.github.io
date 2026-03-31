0b8ae00a93df0e4db22aef74d001c665463ffaa3
desmondwilliams-netizen <desmond.williams@jeffersonrise.org>
2026-03-31 17:03:23 +0000

    circuarit debug

--- fsd-projects/circularity/js/init.js
+++ fsd-projects/circularity/js/init.js
@@ -75,12 +75,11 @@ var init = function (window) {
             // TODO 8 / TODO 9 : Iterate over the array
            for (var  i = 0; i < circles.length; i++) { 
         }
-          for (var i = 0; i < circles.length; i++){
-         physikz.updatePosition(circles[i]);
-
-        game.checkCirclePosition(circles[i]);
-
-          }
+        }
+          //for (var i = 0; i < circles.length; i++){
+         //physikz.updatePosition(circles[i]);
+        //game.checkCirclePosition(circles[i]);
+          //}
         /* 
         This Function should check the position of a circle that is passed to the 
         Function. If that circle drifts off the screen, this Function should move
@@ -136,5 +135,4 @@ if((typeof process !== 'undefined') &&
     (typeof process.versions.node !== 'undefined')) {
     // here, export any references you need for tests //
     module.exports = init;
-}
 }
